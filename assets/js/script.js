e7d1a7afe63899ee3b20123fdf1670f36bd4c432
Ahmed444440 <69533288+Ahmed444440@users.noreply.github.com>
2024-12-10 17:14:38 +0200

    وصف التعديلات هنا

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,6 +23,23 @@ $(function ($) {
         el: ".swiper-pagination",
         clickable: true,
       },
+      breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20
+        },
+        // when window width is >= 480px
+        480: {
+          slidesPerView: 1,
+          spaceBetween: 30
+        },
+        // when window width is >= 640px
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 40
+        }
+      }
     });
  //===== Menu More Categories
     
